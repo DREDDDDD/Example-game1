e189a61316d4313473f44bb723ac6c0575dd744c
DREDDDDD <kosmasebastian@gmail.com>
2023-02-09 19:54:38 +0100

    Add after trying to do the Quest (prolly fucked something up)

--- Example-game1/Assets/Scripts/Interactable/CharacterInteractable.cs
+++ Example-game1/Assets/Scripts/Interactable/CharacterInteractable.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CharacterInteractable : Interactable 
-{
+{  
     public Text Dialogue;
+    
     public override void Interact()
     {
         base.Interact();
-        Dialogue.text = "elo";
+        Dialogue.text = "Siema, mam na sprzeda¿ crack.";
+        
+        
 
     }
     // Start is called before the first frame update
